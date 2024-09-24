e840a6a2bf28e785c6e201c7cfa4a0c9c6cdd93f
Danil Kostin <danilkostin150705@gmail.com>
2024-09-24 09:35:19 +0300

    feat(ts):   task 2  modified

--- 2.ts
+++ 2.ts
@@ -1,24 +1,21 @@
 // 1) Написать тип для функции, которая принимает один аргумент - число или строку. Если пришло число - возвращает {d: 1}, если пришла строка {z: 2}
 
-type TInput = number | string;
-type TReturn<T> = T extends number
+type Input = number | string;
+type Return<T extends Input> = T extends number
   ? { d: 1 }
-  : T extends string
-  ? { z: 2 }
-  : never;
+  : { z: 2 }
 
-function f<T extends TInput>(arg: T) {
-  if (typeof arg === "number") return { d: 1 } as TReturn<T>;
-  return { z: 2 } as TReturn<T>;
+function f<T extends Input>(arg: T): Return<T> { 
+  if (typeof arg === "number") return { d: 1 } as Return<T>;  // не работает без приведения типов
+  return { z: 2 } as Return<T>;  // не работает без приведения типов
 }
 
-// или
-function f2(arg: number | string) {
-  if (typeof arg === "number") return { d: 1 };
-  return { z: 2 };
-}
-f2(1).d;
-f2("t").z;
+console.log(f(1).d)
+console.log(f("123").z)
+// console.log(f(1).z) // error
+// console.log(f("123").d) // error
+// console.log(f([1, 2 ,3])) // error
+
 
 // 2) Реализовать класс SomeClass:
 class SomeClass<T extends { [key: string]: any }> {
